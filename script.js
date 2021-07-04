a886f276d3aabfbea57cf03e7e9b96277e326b07
hellomohitsangwan <hellomohitsangwan@gmail.com>
2021-07-04 17:56:09 +0530

    services ard modifies

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".swiper-container", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 70,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
